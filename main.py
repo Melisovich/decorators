78529be74ca4d8edddb522ea305f0bdd783ebf11
Melisovich <emkamelsov57@gmail.com>
2023-01-21 21:03:45 +0600

    classwork

---
+++ main.py
@@ -0,0 +1,25 @@
+# from functools import wraps
+
+
+# def upper_text(func):
+#     @wraps(func)
+#     def wrapper():
+#         result = func()
+
+#         return result.upper()
+    
+#     return wrapper
+
+
+# @upper_text
+# def say_hi():
+#     return 'Всем привет!'
+
+# print(say_hi)
+
+
+# my_number = ‘+996700123456’
+# Далее создайте переменную my_number_hidden и зашифруйте переменную my_number так, чтобы остались только последние 2 числа, а остальные все были заменены на символ # например
+# ‘###########56’
+# все реализовать через декоратор
+
